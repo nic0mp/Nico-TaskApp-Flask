8e4a5656a0d5d8a9bd9b116d722fd0f9284018ff
nic0mp <nico.mp215@gmail.com>
2021-12-07 23:36:35 -0600

    created index ,import sqlalchemy

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
 
 @app.route('/')
 def index():
-    return 'Hey yo'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'ABOUT'
+
+
 # if __name__=="__main__":
 #     app.run(debug=True)
